bc9750e062f4e2563b708f45b29ad72ab721b6a5
SeinopSys <seinopsys@gmail.com>
2016-12-19 21:23:28 +0100

    Actualize first run screen

--- js/script.js
+++ js/script.js
@@ -473,7 +473,7 @@ $(function(){
 	if (!LStorage.has('firstrun')){
 		$(document.createElement('div'))
 			.attr('id','dialog')
-			.html('<div id="dialog-inner"><h1>Welcome to Derpi-New Tab</h1><p>To access the settings, move your cursor to the left edge of the browser window.<br><span style="color:rgba(255,255,255,.5)">(this message is only displayed once)</span></p></div>')
+			.html('<div id="dialog-inner"><h1>Welcome to Derpi-New Tab</h1><p>To access the settings click the <i class="material-icons">menu</i> icon in the bottom left of the browser window.<br><span style="color:rgba(255,255,255,.5)">(this message is only displayed once)</span></p></div>')
 			.children()
 			.append($(document.createElement('button')).text('Got it').on('click',function(e){
 				e.preventDefault();
